f54bdd2e682d2c7a4e4266e777ade04de08e923e
Akash Suryavanshi <128505050+imakashsuryavanshi@users.noreply.github.com>
2023-12-25 13:43:26 +0530

    Initial commit

---
+++ RE.js
@@ -0,0 +1,27 @@
+function ab(){
+    document.getElementById("sk").src="./images/10.avif";
+}
+function a(){
+    document.getElementById("sk").src="./images/10.1.avif";
+}
+function b(){
+    document.getElementById("sk").src="./images/10.2.avif";
+}
+function c(){
+    document.getElementById("sk").src="./images/10.3.avif";
+}
+function d(){
+    document.getElementById("sk").src="./images/10.4.avif";
+}
+function e(){
+    document.getElementById("sk").src="./images/10.5.avif";
+}
+function f(){
+    document.getElementById("sk").src="./images/10.6.avif";
+}
+function g(){
+    document.getElementById("sk").src="./images/10.7.avif";
+}
+function h(){
+    document.getElementById("sk").src="./images/10.8.avif";
+}
